d2ccd67ddccde31927a542a8f3acc98d1075ce85
tawhid.hassan <tawhid.tirzok@gmail.com>
2023-10-09 13:24:55 +0600

    remove redundant codes

--- src/main/java/com/demo/redistest/controller/HelloRedisController.java
+++ src/main/java/com/demo/redistest/controller/HelloRedisController.java
@@ -1,14 +1,11 @@
 package com.demo.redistest.controller;
 
 import com.demo.redistest.Constant;
-import com.demo.redistest.dto.Message;
 import com.demo.redistest.service.CacheService;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,11 +19,6 @@ public class HelloRedisController {
     @Autowired
     CacheService cacheService;
 
-    /*@GetMapping("/strings")
-    //@ResponseStatus(HttpStatus.CREATED)
-    public Map<String, List<String>> loadDummyData() throws JsonProcessingException {
-        return cacheService.loadData();
-    }*/
     @GetMapping("/load")
     public ResponseEntity<Map<String, List<String>>> loadDummyData() throws JsonProcessingException {
         Map<String, List<String>> data = cacheService.loadData();
@@ -40,20 +32,8 @@ public class HelloRedisController {
     }
 
     @PostMapping("/update")
-    public ResponseEntity<?> updateKey(@RequestBody JsonNode requestBody) throws JsonProcessingException {
+    public ResponseEntity<List<String>> updateKey(@RequestBody JsonNode requestBody) throws JsonProcessingException {
         List<String> data = cacheService.updateValueForKey(requestBody);
         return ResponseEntity.ok(data);
     }
-
-
-    /*@PostMapping("/strings")
-    public Object updateStatus(@RequestBody Message message) {
-        cacheService.replace(Constant.KEY_TXN, message);
-        return cacheService.<Map<String, List<Message>>>find(Constant.KEY_TXN, new TypeReference<>() {});
-    }
-
-    @GetMapping("/check-dlr")
-    public Object getAll() {
-        return cacheService.<Map<String, List<Message>>>find(Constant.KEY_TXN, new TypeReference<>() {});
-    }*/
 }
--- src/main/java/com/demo/redistest/service/CacheService.java
+++ src/main/java/com/demo/redistest/service/CacheService.java
@@ -4,12 +4,10 @@ import com.demo.redistest.Constant;
 import com.demo.redistest.dto.Message;
 import com.demo.redistest.repository.CacheRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.HashOperations;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
